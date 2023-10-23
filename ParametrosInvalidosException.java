b65e786ab166df464d128508ef77c6a04c2692a7
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2023-10-22 22:51:15 -0300

    criação da classe exception

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+class ParametrosInvalidosException extends Exception{
+    ParametrosInvalidosException(){
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+    }
+   
+}
